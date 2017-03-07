8c9e0ddef279f284ff6d38f15294d4ff6945eca6
Mike North <northm17@mail.wlu.edu>
2017-03-07 13:14:18 -0500

    started loading ncsr4 and trying to figure out the function for bar figure

--- barPlot/server.R
+++ barPlot/server.R
@@ -1,4 +1,16 @@
 library(shiny)
+library(ggplot2)
+
+ncsr4 <- ncsr4
+
+selectInput("", label = "Risk Factor:", choices = c("tenure", "exposure"))
+
+renderPlot({
+   ncsr4 %>%
+    group_by(input$group, carrier) %>%
+    summarize(ave_delay = mean(sum(arr_delay + dep_delay, na.rm = TRUE))) %>%
+    ggplot(aes(input$group, ave_delay, colour = carrier)) + geom_bar(stat = "identity")
+})
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {

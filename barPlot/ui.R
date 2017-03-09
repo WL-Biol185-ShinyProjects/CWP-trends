49eae7935a946c0442b449c61e625b3ba7bf6ebe
Mike North <northm17@mail.wlu.edu>
2017-03-09 13:04:03 -0500

    trying to get connection between ui.R and server.R for exposure plot

--- barPlot/ui.R
+++ barPlot/ui.R
@@ -2,24 +2,22 @@ library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
-  
-  # Application title
-  titlePanel("Old Faithful Geyser Data"),
-  
-  # Sidebar with a slider input for number of bins
-  # Add in actionButton?
-  sidebarLayout(
+  fluidRow(
+    column(4,
+    sidebarLayout(
     sidebarPanel(
-      sliderInput("bins",
-                  "Number of bins:",
-                  min = 1,
-                  max = 100,
-                  value = 30)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
-)
+  sliderInput("slider1", 
+              label = h3("Coal Exposure (gram-hours per cubic meter)"),
+              min = 0, 
+              max = 100,
+              value = c(40, 60))
+                )
+                 )
+         )
+         ),
+  mainPanel(
+  plotOutput("expPlot")
+           )
+        )
+
+ 

ea3d3d4e755339ebd5cb7a81191878e8d5daf85d
Mike North <northm17@mail.wlu.edu>
2017-03-14 12:44:41 -0400

    added ordered stacked bars to bar plot on server.r

--- barPlot/server.R
+++ barPlot/server.R
@@ -2,20 +2,20 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 ncsr4 <- read.csv("data/ncsr4_clean.csv")
-
+ncsr4$REGION <- factor(ncsr4$REGION, levels = c("HI RANK", "MED RANK", "LOW RANK"))
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
   output$range <- renderPrint({input$slider2})
 
-  
+ 
   renderPlot({
     ncsr4 %>%
-      filter(PMF == "PMF+", EXP == output$range) %>%
+      filter(PMF == "PMF+", EXP > 11) %>%
       group_by(AGE) %>%
-      count(AGE) %>%
-      ggplot(aes(AGE, n)) +
-      geom_bar(stat = "identity")
+      count(AGE, REGION) %>%
+      ggplot(aes(AGE, n, fill = REGION)) +
+      geom_bar(stat = "identity", position = "stack")
         
     
    

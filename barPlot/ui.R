6060f4880cd1e392afacf24349e66a2d01f373bc
Mike North <northm17@mail.wlu.edu>
2017-03-14 12:53:52 -0400

    first attempt to add in disease drop down for bar plot. also worked on connection between ui.r and server.r

--- barPlot/ui.R
+++ barPlot/ui.R
@@ -10,10 +10,15 @@ fluidPage(
               min = 0, 
               max = 200,
               value = c(0, 200)
-                 )
+                 ),
+      selectInput("select1", label = h3("Select Disease"), 
+                  choices = list("CWP-Category 1 " = 1, "CWP-Category 2" = 2, "PMF" = 3), 
+                  selected = 1)
                 ),
+    
       mainPanel(
         plotOutput("expPlot") )
+    
               )
                )
         

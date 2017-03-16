5a08e8ea16fd172e4d92b96a21dfa3642609d639
John Bowman <bowmanj17@mail.wlu.edu>
2017-03-16 13:25:29 -0400

    Add in disease column to rapidProg and save in Rdata file.

--- mapPlot/server.R
+++ mapPlot/server.R
@@ -1,18 +1,29 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-ncsr4 <- read.csv("../data/ncsr4_clean.csv")
-ncsr4$REGION <- factor(ncsr4$REGION, levels = c("HI RANK", "MED RANK", "LOW RANK"))
 
-# Define server logic required to draw a histogram
+#Load in, rename region data relating region to state
+load("mapPlot/data/serverData.RData")
+regionData <- df_join
+remove(df_join)
+
+#Load in polygon info for country and state in U.S.
+uscounties <- rgdal::readOGR("mapPlot/data/counties_5m.json", "OGRGeoJSON")
+usstates   <- rgdal::readOGR("mapPlot/data/state_5m.json", "OGRGeoJSON")
+
+#Load in rapid progression data
+rapidProg <- read.csv("mapPlot/data/rapidProgression_clean.csv")
+
+------------------------- not finished below-----------------------------------
+# Define server logic required to draw map plot
 shinyServer(function(input, output) {
   
   
   
   output$expPlot <- renderPlot({
-    ncsr4 %>%
-      filter(PMF == "PMF+", EXP >= input$slider1[1], EXP <= input$slider1[2]) %>%
-      group_by(AGE) %>%
+    rapidProg%>%
+      filter(XRAY_YEAR >= input$slider1[1], XRAY_YEAR <= input$slider1[2]) %>%
+      group_by(REGION) %>%
       count(AGE, REGION) %>%
       ggplot(aes(AGE, n, fill = REGION)) +
       geom_bar(stat = "identity", position = "stack")

777849f104cc13b704af00be20b8f49ac6ebede3
Mike North <northm17@mail.wlu.edu>
2017-03-21 12:16:08 -0400

    added nav bar for exposure plots and for temporal line plot

--- finalCompiled/ui.R
+++ finalCompiled/ui.R
@@ -1,34 +1,39 @@
 library(shiny)
 
 
-fluidPage(theme = shinythemes::shinytheme("superhero"),
-  titlePanel("Trends in Coal Dust-related Lung Disease"),
-  sidebarLayout(position = "right",
-    sidebarPanel(
-      sliderInput("slider1", 
-                  label = h3("Coal Exposure (gram-hours per cubic meter)"),
-                  min = 0, 
-                  max = 200,
-                  value = c(0, 200),
-                  round = TRUE),
-      
-      selectInput("select1", label = h3("Disease Box"), 
-                  choices = list("CWP" = "CWP", "PMF" = "PMF"), 
-                  selected = 1)
-      
-      
-                 ),
-      
-  mainPanel(
-      plotOutput("expPlot"),
-      plotOutput("cwpExpPlot"),
-      tabsetPanel(type = "tabs",
-                  tabPanel("Plot", plotOutput("linePlot")),
-                  tabPanel("Summary", verbatimTextOutput("summary"))
-    
-              )
-         )
-
-
-)
-)
+navbarPage(theme = shinythemes::shinytheme("superhero"), "Trends",
+           tabPanel("Exposure Plots",
+                    sidebarLayout(
+                      sidebarPanel(
+                        sliderInput("slider1", 
+                                    label = h3("Coal Exposure (gram-hours per cubic meter)"),
+                                    min = 0, 
+                                    max = 200,
+                                    value = c(0, 200),
+                                    round = TRUE)
+                                   ),
+                        mainPanel(
+                          plotOutput("expPlot"),
+                          plotOutput("cwpExpPlot")
+                                 )
+                                )
+                    ),
+                    
+           tabPanel("Temporal Plots",
+                    sidebarLayout(
+                      sidebarPanel(
+                        selectInput("select1", label = h3("Disease Box"), 
+                                    choices = list("CWP" = "CWP", "PMF" = "PMF"), 
+                                    selected = 1)
+                                  ),
+                      mainPanel(
+                          tabsetPanel(type = "tabs",
+                              tabPanel("Plot", plotOutput("linePlot")),
+                              tabPanel("Summary", verbatimTextOutput("summary"))
+                                     )
+                                )
+                               )
+                    )
+          )
+                    
+                            

b4168cb466e6f778c766f0044e02daac8855c65b
Mike North <northm17@mail.wlu.edu>
2017-03-21 12:43:53 -0400

    reformatted nav bar so that starts on first tab. added prelim text for each figure.

--- finalCompiled/ui.R
+++ finalCompiled/ui.R
@@ -1,19 +1,34 @@
 library(shiny)
 
-
-navbarPage(theme = shinythemes::shinytheme("superhero"), "Trends",
+navbarPage(theme = shinythemes::shinytheme("superhero"), 
+           "Trends",
+           selected = "Exposure Plots",
+           titlePanel(
+                      h1 ("CWP and PMF Trends", align = "center")
+                     ),
            tabPanel("Exposure Plots",
                     sidebarLayout(
-                      sidebarPanel(
+                      sidebarPanel("Choose:",
                         sliderInput("slider1", 
-                                    label = h3("Coal Exposure (gram-hours per cubic meter)"),
+                                    label = ("Coal Exposure (gram-hours per cubic meter)"),
                                     min = 0, 
                                     max = 200,
                                     value = c(0, 200),
                                     round = TRUE)
                                    ),
                         mainPanel(
+                          strong("Figure 2"),
+                          p("The number of individuals diagnosed with PMF is 
+                            displayed as a function of age. Stacked bars represent
+                            exposure to three types of coal: high rank, medium rank,
+                            and low rank."),
                           plotOutput("expPlot"),
+                          br(),
+                          strong("Figure 3"),
+                          p("The number of individuals diagnosed with CWP is 
+                            displayed as a function of age. Stacked bars represent 
+                            exposure to three types of coal: high rank, medium rank,
+                            and low rank."),
                           plotOutput("cwpExpPlot")
                                  )
                                 )
@@ -26,8 +41,10 @@ navbarPage(theme = shinythemes::shinytheme("superhero"), "Trends",
                                     choices = list("CWP" = "CWP", "PMF" = "PMF"), 
                                     selected = 1)
                                   ),
-                      mainPanel(
-                          tabsetPanel(type = "tabs",
+                    mainPanel(
+                        strong("Figure 4"),
+                        p("The number of individuals with CWP or PMF is displayed as a function of time. Each line indicates a different coal region."),
+                        tabsetPanel(type = "pills",
                               tabPanel("Plot", plotOutput("linePlot")),
                               tabPanel("Summary", verbatimTextOutput("summary"))
                                      )

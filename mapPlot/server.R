15a47aef5c5217c0c579c39b93520f3a4ea544dc
John Bowman <bowmanj17@mail.wlu.edu>
2017-03-23 13:54:13 -0400

    Finally got map to run, need to fully integrate into serve and ui code, and fix color scheme. Need to make sure the bin/numberic quantile color schemes aren't losing the value of 1, which they are right now.

--- mapPlot/server.R
+++ mapPlot/server.R
@@ -1,18 +1,38 @@
 library(shiny)
-library(ggplot2)
 library(dplyr)
+library(leaflet)
 
 #Load in, rename region data relating region to state
 load("mapPlot/data/serverData.RData")
-regionData <- df_join
-remove(df_join)
+
+#in toMerge: n = number of the disease for that year for that region
 
 #Load in polygon info for country and state in U.S.
-uscounties <- rgdal::readOGR("mapPlot/data/counties_5m.json", "OGRGeoJSON")
 usstates   <- rgdal::readOGR("mapPlot/data/state_5m.json", "OGRGeoJSON")
+uscounties <- rgdal::readOGR("mapPlot/data/counties_5m.json", "OGRGeoJSON")
 
-#Load in rapid progression data
-rapidProg <- read.csv("mapPlot/data/rapidProgression_clean.csv")
+value <- toMerge %>%
+            filter(XRAY_YEAR >= input$slider[1], XRAY_YEAR <= input$slider[2]) %>%
+            subset(DISEASE == input$select1) %>%
+            group_by(REGION, XRAY_YEAR) %>%
+            summarise(total = mean(n)) %>%
+            group_by(REGION) %>%
+            summarise(sumTotal = sum(total))
+            
+toMerge$XRAY_YEAR <- NULL
+toMerge$n         <- NULL
+
+filt_toMerge <- filter(toMerge, DISEASE == input$select1)
+join         <- left_join(filt_toMerge, value, by = "REGION")
+dd.join      <- join[!duplicated(join), ]
+          
+uscounties@data <- left_join(uscounties@data, dd.join, by = "GEO_ID")
+uscounties@data$sumTotal[is.na(uscounties@data$sumTotal)] <- 0
+
+leaflet(data = uscounties) %>%
+  addTiles() %>%
+  addPolygons(color = ~colorNumeric("YlOrRd", sumTotal)(sumTotal), fillOpacity = 0.5, weight = 1) %>%
+  addPolygons(data = usstates, color = "black", weight = 5, fillOpacity = 0)
 
 ------------------------- not finished below-----------------------------------
 # Define server logic required to draw map plot
--- mapPlot/ui.R
+++ mapPlot/ui.R
@@ -5,18 +5,24 @@ fluidPage(
   titlePanel("Approach"),
   sidebarLayout(
     sidebarPanel(
-      sliderInput("slider1", 
-                  label = h3("Coal Exposure (gram-hours per cubic meter)"),
-                  min = 0, 
-                  max = 200,
-                  value = c(0, 200),
-                  round = TRUE
-      )
-    ),
+      sliderInput( "slider1"
+                 , label = h3("Coal Exposure (gram-hours per cubic meter)")
+                 , min = 0
+                 , max = 200
+                 , value = c(0, 200)
+                 , round = TRUE
+                 ),
+      
+      selectInput( "select1"
+                 , label = h3("Disease Box")
+                 , choices = list("CWP" = "CWP", "PMF" = "PMF")
+                 , selected = 1
+                 )
+                ),
     
     mainPanel(
-      plotOutput("expPlot") )
+      plotOutput("expPlot")
+             )
     
-  )
-)
-
+               )
+        )
